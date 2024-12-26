d5f581dc61cbd1e65f58c0da3530a9f01d0112aa
Robert Wang <wangrob@amazon.com>
2024-12-25 22:01:26 -0800

    back up old version and move v2 to current version

--- metadata-index/backend/config.py
+++ metadata-index/backend/config.py
@@ -1,10 +1,9 @@
-CDK_APP_NAME = "almvAhiIndex"
+#CDK_APP_NAME = "[APP-STACK_NAME]"
+CDK_APP_NAME = "metadata-index-v2"
 
 #AHI settings: This should be set regardless of the mode.
 #The datstore ARN of the AHI datastore. You can find this value in the AHI Web console on your AWS account.
-AHI_DATASTORE_ARN = "arn:aws:medical-imaging:us-east-1:767397949481:datastore/617156b243b84117adb9d33dd32588f7"
-#The bucket specified as the output s3 location for the AHI import jobs.
-AHI_IMPORT_OUPUT_BUCKET_ARN = "arn:aws:s3:::alim-us-east1-dev-imaging-output"
+AHI_DATASTORE_ARN = "arn:aws:medical-imaging:[REGION]:[ACCOUNT_NUMBER]:datastore/[DATASTORE_ID]"
 
 #Note that to use RDBMS mode requires to havve a VPC. Make sure to set USE_PVC to True in the VPC config below.
 RDBMS_CONFIG = {
@@ -13,17 +12,17 @@ RDBMS_CONFIG = {
     "populate_instance_level" : True,
     #Wether to populate the frame level tags in the frame table or not. If this is set to True, the instance level tags will also be populated.
     "populate_frame_level" : True,
-    "db_name" : "almvahiindex",
+    "db_name" : "ahiindex",
     "min_acu_capacity" : 1,
     "max_acu_capacity" : 16
 }
 
 DATALAKE_CONFIG = {
-    "enabled" : False,
+    "enabled" : True,
     #Wether to populate the instance level tags in the AHI datalake or not.
     "populate_instance_level" : True,
     #If the parameter destination_bucket_name is left empty the solution will create a random bucket name. If you choose to use a bucket name of your own, make sure that the bucket name is not already in use.
-    "destination_bucket_name" : "ahilake",
+    "destination_bucket_name" : "",
     "deploy_glue_default_config" : True,
 }
 
@@ -89,7 +88,7 @@ LAMBDA_CONFIG = {
         "handler": "lambda_handler",
         "index": "index",
         "timeout": 2,
-        "memory": 3008,
+        "memory": 4096,
         "layers": [ "mysqlConnector", "mysqlConnectionFactory" , "AHIClientFactory"],
         "reserved_concurrency": 0,
         "envs": {},
